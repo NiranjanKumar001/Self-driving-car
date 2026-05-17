3b122f1e15eec911556193d4c428958d5fd77a55
niranjan-acorn-globus <niranjan@acornglobus.com>
2026-05-17 19:39:07 +0530

    car with sensor to detect borders and obj is fully done

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const canvas=document.getElementById("myCanvas");
 canvas.width=200;
 
-const ctx=canvas.getContext("2d");
-const road= new Road(canvas.width/2,canvas.width);
+const ctx = canvas.getContext("2d");
+const road=new Road(canvas.width/2,canvas.width*0.9);
 const car=new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
-car.draw(ctx);
 
 function animate(){
-    car.update();
+    car.update(road.borders);
 
     canvas.height=window.innerHeight;
 
@@ -20,6 +19,5 @@ function animate(){
     car.draw(ctx);
 
     ctx.restore();
-
     requestAnimationFrame(animate);
 }
